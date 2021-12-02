2b5b1548d59c47624cbde9dcf3415f70810b6896
spacedawn96 <spacedawn96@gmail.com>
2021-12-02 14:19:39 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  experimental: {
+    reactRoot: true,
+    concurrentFeatures: true,
+    serverComponents: true,
+  },
+};
